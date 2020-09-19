b3aa0522db00ffe2618bb69b0c7aef0026964a6c
Meredith McIntosh <meredith@mcintosh.io>
2020-09-19 16:10:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -75,8 +75,6 @@ def adding_matz
         :languages => ["C"]
       }
     }
-  # programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
-  # programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
   programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
   programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
   
