c4111688dd6304519c118bda655a897f14d49b25
Meredith McIntosh <meredith@mcintosh.io>
2020-09-19 15:46:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,8 +1,7 @@
-
 def hopper
 	programmer_hash = 
- 		{
-      :grace_hopper => {
+		{
+      :grace_hopper => { # Added Bracket
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
       },
@@ -15,14 +14,13 @@ def hopper
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:grace_hopper] # get value of grace hopper key in hash
 end
+hopper # run the function
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
- 		{
+  programmer_hash = 
+		{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -36,12 +34,14 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
+alan_kay_is_known_for
+
 
 def dennis_ritchies_language
 	programmer_hash = 
- 		{
+		{
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -55,19 +55,13 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
+dennis_ritchies_language
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
- 		{
+		{ 
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,49 +75,52 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  # programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
+  # programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
+  programmer_hash[:yukihiro_matsumoto][:known_for]="Ruby"
+  programmer_hash[:yukihiro_matsumoto][:languages]=["LISP", "C"]
+  puts programmer_hash
 end
+adding_matz
 
-def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
+# def changing_alan
+#   programmer_hash = 
+# 		{
+#       :grace_hopper => {
+#         :known_for => "COBOL",
+#         :languages => ["COBOL", "FORTRAN"]
+#       },
+#       :alan_kay => {
+#         :known_for => "Object Orientation",
+#         :languages => ["Smalltalk", "LISP"]
+#       },
+#       :dennis_ritchie => {
+#         :known_for => "Unix",
+#         :languages => ["C"]
+#       }
+#     }
+#   programmer_hash[:alan_kay][:known_for]="GUI"
+#   puts programmer_hash
+# end
+# changing_alan
 
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
-end
-
-def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      }
-    }
-
-end
+# def adding_to_dennis
+# 	programmer_hash = 
+# 		{
+#       :grace_hopper => {
+#         :known_for => "COBOL",
+#         :languages => ["COBOL", "FORTRAN"]
+#       },
+#       :alan_kay => {
+#         :known_for => "Object Orientation",
+#         :languages => ["Smalltalk", "LISP"]
+#       },
+#       :dennis_ritchie => {
+#         :known_for => "Unix",
+#         :languages => ["C"]
+#       }
+#     }
+#   programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
+#   puts programmer_hash
+# end
+# adding_to_dennis
