8c677947f12494b6ea72266da379d9f4294d7c78
Meredith McIntosh <meredith@mcintosh.io>
2020-09-19 16:20:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,23 +105,23 @@ def changing_alan
 end
 changing_alan
 
-# def adding_to_dennis
-# 	programmer_hash = 
-# 		{
-#       :grace_hopper => {
-#         :known_for => "COBOL",
-#         :languages => ["COBOL", "FORTRAN"]
-#       },
-#       :alan_kay => {
-#         :known_for => "Object Orientation",
-#         :languages => ["Smalltalk", "LISP"]
-#       },
-#       :dennis_ritchie => {
-#         :known_for => "Unix",
-#         :languages => ["C"]
-#       }
-#     }
-#   programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
-#   puts programmer_hash
-# end
-# adding_to_dennis
+def adding_to_dennis
+	programmer_hash = 
+		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+  programmer_hash[:dennis_ritchie][:languages][1]="Assembly"
+  puts programmer_hash
+end
+adding_to_dennis
